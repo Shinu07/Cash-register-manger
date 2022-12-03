bd98ad91368face10f9ba5330071dbab18bf5b85
srjp <Official.soumyaranjan.07@gmail.com>
2022-12-03 13:25:18 +0530

    fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
 const checkButton = document.querySelector("#check-button");
-const message = document.querySelector("#error-message");
+const message = document.querySelector("#emessage");
 const noOfNotes = document.querySelectorAll(".no-of-notes");
 
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
@@ -10,9 +10,9 @@ checkButton.addEventListener("click", function validateBillAndCashAmount() {
   hideMessage();
   if (billAmount.value > 0) {
     if (cashGiven.value >= billAmount.value) {
-      const amountToBeReturned =  cashGiven.value - billAmount.value; 
+      const amountToBeReturned =  cashGiven.value - billAmount.value;
       calculateChange(amountToBeReturned);
-    } else { 
+    } else {
       showMessage("Do you wanna wash plates?");
     }
   } else {
